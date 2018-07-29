e49824f16e8d104a77f2dd81b880b8ade19a07e7
Ismael Correa <ismael.correa.castro@gmail.com>
2018-07-29 16:33:57 -0400

    files of other libraries deleted

--- CHANGELOG.md
+++
@@ -1,1 +0,0 @@
-[8-10-2017] namespace names physics, chemistry, biology, math and informatics replace by namespace msci
--- tests/main.cpp
+++
@@ -1,4 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include "catch/catch.hpp"
-//#include "test_dimensions.hpp"
-#include "test_units.hpp"

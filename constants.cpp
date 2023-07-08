ebaa9e491cc7fddb753c739585148cab8e78e1e0
Ismael Correa <ismael.correa.castro@gmail.com>
2023-07-08 14:04:23 -0400

    constants.cpp file

---
+++ constants.cpp
@@ -0,0 +1,10 @@
+#include "constants.hpp"
+
+using namespace std;
+
+namespace
+{
+	const mass electron_mass = mass("9.10938356 * 10^-31 kg");
+	const mass proton_mass = mass("0 g");
+	const mass neutron_mass = mass("0 g");
+}

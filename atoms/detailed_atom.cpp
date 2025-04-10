527fc9da010b19844ddd4bea7ecf0a7fad7a835e
Ismael Correa <ismael.correa.castro@gmail.com>
2025-04-10 14:37:58 -0400

    datom class renamed to detailed_atom class, file datoms_ia.hpp deleted because it's no longer needed

---
+++ atoms/detailed_atom.cpp
@@ -0,0 +1,14 @@
+#include "./detailed_atom.hpp"
+
+using namespace std;
+
+namespace scifir
+{
+	detailed_atom::detailed_atom()
+	{
+	}
+
+	detailed_atom::detailed_atom(const string& new_electronic_configuration)
+	{
+	}
+}
---
+++ atoms/detailed_atom.hpp
@@ -0,0 +1,18 @@
+#ifndef SCIFIR_CHEMISTRY_ATOMS_DATOM_HPP_INCLUDED
+#define SCIFIR_CHEMISTRY_ATOMS_DATOM_HPP_INCLUDED
+
+#include <string>
+
+using namespace std;
+
+namespace scifir
+{
+	class detailed_atom
+	{
+		public:
+			detailed_atom();
+			detailed_atom(const string&);
+	};
+}
+
+#endif // SCIFIR_CHEMISTRY_ATOMS_DATOM_HPP_INCLUDED

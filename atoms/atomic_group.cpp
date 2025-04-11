95241df24c568cfc7824f232f29d02d273409548
Ismael Correa <ismael.correa.castro@gmail.com>
2025-04-11 15:46:21 -0400

    atomic_group class moved to atoms folder

---
+++ atoms/atomic_group.cpp
@@ -0,0 +1,10 @@
+#include "./atomic_group.hpp"
+
+using namespace std;
+
+namespace scifir
+{
+	atomic_group::atomic_group()
+	{
+	}
+}
---
+++ atoms/atomic_group.hpp
@@ -0,0 +1,15 @@
+#ifndef SCIFIR_CHEMISTRY_MOLECULES_ATOMIC_GROUP_HPP_INCLUDED
+#define SCIFIR_CHEMISTRY_MOLECULES_ATOMIC_GROUP_HPP_INCLUDED
+
+using namespace std;
+
+namespace scifir
+{
+	class atomic_group
+	{
+		public:
+			atomic_group();
+	};
+}
+
+#endif // SCIFIR_CHEMISTRY_MOLECULES_ATOMIC_GROUP_HPP_INCLUDED

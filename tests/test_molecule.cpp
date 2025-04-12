14946f1ca5059d1bb9c6bca16c2604d0ecf443a7
Ismael Correa <ismael.correa.castro@gmail.com>
2025-04-11 23:45:00 -0400

    Test for propane added

--- tests/test_molecule.cpp
+++ tests/test_molecule.cpp
@@ -18,6 +18,8 @@ TEST_CASE("molecule class","Full test of molecule class") {
 		scifir::molecule a({ atom(atom::C),atom(atom::H),atom(atom::H),atom(atom::H),atom(atom::H) },{atomic_bond_builder(0,1),atomic_bond_builder(0,2),atomic_bond_builder(0,3),atomic_bond_builder(0,4)});
 		CHECK(a.get_canonical_formula() == "CH4");
 		scifir::molecule b({ atom::C,atom::H,atom::H,atom::H,atom::H },{ {0,1},{0,2},{0,3},{0,4} });
-		CHECK(a.get_canonical_formula() == "CH4");
+		CHECK(b.get_canonical_formula() == "CH4");
+		scifir::molecule c({ atom::C,atom::C,atom::C,atom::H,atom::H,atom::H,atom::H,atom::H,atom::H,atom::H,atom::H },{ {0,3},{0,4},{0,5},{1,6},{1,7},{2,8},{2,9},{2,10} });
+		CHECK(c.get_canonical_formula() == "C3H8");
 	}
 }
